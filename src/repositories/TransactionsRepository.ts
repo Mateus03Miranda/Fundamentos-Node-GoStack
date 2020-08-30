dc4b2fbff279f9cb9c8422bf349540e2b81d99c9
Mateus Miranda <mateusm.miranda80@gmail.com>
2020-08-30 14:50:25 -0300

    finalizaçao do Desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+/* eslint-disable class-methods-use-this */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const somar = (valoratual: number, proximovalor: number): number =>
+      valoratual + proximovalor;
+    const income = this.transactions.map(transaction => {
+      if (transaction.type === 'income') {
+        return transaction.value;
+      }
+      return 0;
+    });
+    income.push(0);
+    const outcome = this.transactions.map(transaction => {
+      if (transaction.type === 'outcome') {
+        return transaction.value;
+      }
+      return 0;
+    });
+    outcome.push(0);
+    const balance = {
+      income: income.reduce(somar),
+      outcome: outcome.reduce(somar),
+      total: income.reduce(somar) - outcome.reduce(somar),
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  // eslint-disable-next-line class-methods-use-this
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,12 @@
+/* eslint-disable class-methods-use-this */
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
@@ -8,8 +15,12 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: RequestDTO): Transaction {
+    // if (this.transactionsRepository.all()) throw Error('Total is menor que 0');
+    const { total } = this.transactionsRepository.getBalance();
+    if (total - value < 0 && type === 'outcome')
+      throw Error('Total is menor que 0');
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
